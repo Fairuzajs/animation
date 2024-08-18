c512e7dfb93d14659d251eb4a66906f1e4b180e0
fairuzajs <yaubasarova.93@mail.ru>
2024-08-18 15:05:40 +0500

    feat: add style

--- js/sky.js
+++ js/sky.js
@@ -83,64 +83,8 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 // 3 
 
-// Подключаем библиотеку Scene.js
 
 
-// Создаем анимацию
-const scene = new Scene({
-  ".module-3[data-typing='presentation']": Scene.typing({ text: "Создание красивых презентаций", duration: 1}),
-  ".module-3[data-typing='websites']": {
-    1.5: Scene.typing({ text: "Создание сайтов", duration: 1}),
-  },
-  ".module-3[data-typing='ai']": {
-    3: Scene.typing({ text: "Работа с искусственным интеллектом", duration: 1}),
-  },
-  ".module-3[data-typing='spreadsheets']": {
-    4.5: Scene.typing({ text: "Электронные таблицы", duration: 1}),
-  },
-  ".module-3[data-typing='marketing']": {
-    6: Scene.typing({ text: "Интернет-маркетинг", duration: 1}),
-  },
-  ".module-3[data-typing='chatbots']": {
-    7.5: Scene.typing({ text: "Создание чат-ботов", duration: 1}),
-  },
-  ".module-3[data-typing='videos']": {
-    9: Scene.typing({ text: "Создание рекламных видеороликов", duration: 1}),
-  },
-  ".module-3[data-typing='3d-design']": {
-    10.5: Scene.typing({ text: "3D-дизайн интерьеров", duration: 1}),
-  },
-  ".module-3[data-typing='blogging']": {
-    12: Scene.typing({ text: "Блогинг", duration: 1}),
-  },
-  ".module-3[data-typing='photo-video']": {
-    13.5: Scene.typing({ text: "Фото и Видео-монтаж", duration: 1}),
-  },
-  ".module-3[data-typing='animation']": {
-    15: Scene.typing({ text: "Создание анимации", duration: 1}),
-  },
-  ".module-3[data-typing='finance']": {
-    16.5: Scene.typing({ text: "Основы финансовой грамотности", duration: 1}),
-  },
-  ".module-3[data-typing='games']": {
-    18: Scene.typing({ text: "Создание игр", duration: 1}),
-  },
-  ".module-3[data-typing='clothing']": {
-    19.5: Scene.typing({ text: "Создание виртуальной одежды", duration: 1}),
-  },
-  ".module-3[data-typing='ecommerce']": {
-    21: Scene.typing({ text: "Создание интернет-магазинов", duration: 1}),
-  },
-  ".module-3[data-typing='programming']": {
-    22.5: Scene.typing({ text: "Программирование", duration: 1}),
-  },
-}, {
-  selector: true,
-});
 
-// Запуск анимации
-scene.setPlaySpeed(1);
-scene.setEasing("ease-in-out");
-scene.setIterationCount("infinite");
-scene.play();
+
 
